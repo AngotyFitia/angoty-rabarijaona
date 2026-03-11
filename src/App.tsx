1eba280598bd6023c12a77a52db6417aa590e476
Angoty <angotyrabarijaona@gmail.com>
2026-03-11 10:38:37 +0300

    setting-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from "@/components/ui/button"
 import Header from "@/components/ui/header"
 import Home from "@/components/ui/home"
 import About from "@/components/ui/about"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="min-h-screen">
