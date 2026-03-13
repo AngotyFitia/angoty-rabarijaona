23509e8dc83975b9ee38bf902efbf3035564cd3a
Angoty <angotyrabarijaona@gmail.com>
2026-03-13 16:29:18 +0300

    skills section
    feat: add hardskills and sofskills

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/ui/header"
 import Home from "@/components/ui/home"
 import About from "@/components/ui/about"
 import Project from './components/ui/projects'
+import Skills from './components/ui/skills'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Home />
         <About />
         <Project />
+        <Skills />
         {/* <section id="about" className="p-10">À propos…</section>
         <section id="projects" className="p-10">Mes projets…</section>
         <section id="contact" className="p-10">Contact…</section> */}
